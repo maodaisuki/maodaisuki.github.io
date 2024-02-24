f83f06be34072a91b6fe1cff039b435e3b4c5d48
maodaisuki <nyanyanyapu@gmail.com>
2024-02-24 22:21:55 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 对应 Github Page 仓库
-  base: '/maodaisuki.github.io',
+  base: '/',
   plugins: [
     vue(),
   ],
