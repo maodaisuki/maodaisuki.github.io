5da675a4d137324ef5c971d458619d6398bd29c4
maodaisuki <nyanyanyapu@gmail.com>
2024-02-24 21:44:32 +0800

    Revert "Update"
    This reverts commit 744c86c51f4b6aaebfe56cd918132a638a40526b.

---
+++ src/components/LinkCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import SNSCard from '../components/SNSCard.vue';
+import SiteCard from '../components/SiteCard.vue';
+</script>
+<template>
+    <div class="link-card">
+        <a-tabs type="capsule" size="medium" justify="true">
+            <a-tab-pane key="1" title="SNS">
+                <SNSCard />
+            </a-tab-pane>
+            <a-tab-pane key="2" title="サイト">
+                <SiteCard />
+            </a-tab-pane>
+        </a-tabs>
+    </div>
+</template>
+<style scoped>
+    .link-card {
+        margin: 10px 0;
+        width: 100%;
+    }
+    .link-card a-tabs a-tab-pane {
+        width: 100%;
+    }
+
+    .arco-tabs-nav-tab-list {
+        width: 100% !important;
+    }
+    .arco-tabs-content {
+        padding-top: 5px !important;
+    }
+</style>
