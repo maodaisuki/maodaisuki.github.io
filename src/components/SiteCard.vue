e8e1b07bd320d6e8079328c42e91d84d6d63866f
maodaisuki <nyanyanyapu@gmail.com>
2024-04-16 22:54:57 +0800

    feat: 删除指向壁打ち的链接

--- src/components/SiteCard.vue
+++ src/components/SiteCard.vue
@@ -25,19 +25,6 @@
             </a-list-item>
         </a-list>
     </a>
-    <a style="text-decoration: none;" target="_blank" href="https://nyanpasu.space/memos">
-        <a-list hoverable="true" size="small">
-            <a-list-item>
-                <a-list-item-meta title="壁打ち" description="ひとりごと">
-                    <template #avatar>
-                        <a-avatar shape="square">
-                            <img alt="avatar" src="https://nyanpasu.space/favicon.ico" />
-                        </a-avatar>
-                    </template>
-                </a-list-item-meta>
-            </a-list-item>
-        </a-list>
-    </a>
     <a style="text-decoration: none;" target="_blank" href="https://memos.nyanpasu.space">
         <a-list hoverable="true" size="small">
             <a-list-item>
