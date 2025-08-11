5f70077d439d1858532007d9473bbb4755235da3
mao <nyanyanyapu@gmail.com>
2025-08-11 20:16:04 +0800

    Update SNSCard.vue

--- src/components/SNSCard.vue
+++ src/components/SNSCard.vue
@@ -12,19 +12,6 @@
             </a-list-item>
         </a-list>
     </a>
-    <a style="text-decoration: none;" target="_blank" href="https://mstdn.jp/@maodaisuki">
-        <a-list hoverable="true" size="small">
-            <a-list-item>
-                <a-list-item-meta title="Mastodon" description="@maodaisuki">
-                    <template #avatar>
-                        <a-avatar shape="square">
-                            <img alt="avatar" src="/images/mastodon-icon.png" />
-                        </a-avatar>
-                    </template>
-                </a-list-item-meta>
-            </a-list-item>
-        </a-list>
-    </a>
     <a style="text-decoration: none;" target="_blank" href="https://www.last.fm/user/mao0_0">
         <a-list hoverable="true" size="small">
             <a-list-item>
@@ -32,19 +19,6 @@
                     <template #avatar>
                         <a-avatar shape="square">
                             <img alt="avatar" src="/images/lastfm-icon.png" />
-                        </a-avatar>
-                    </template>
-                </a-list-item-meta>
-            </a-list-item>
-        </a-list>
-    </a>
-    <a style="text-decoration: none;" target="_blank" href="https://open.spotify.com/user/315uke2awp6jeqw4y6eyi6wxa6ni">
-        <a-list hoverable="true" size="small">
-            <a-list-item>
-                <a-list-item-meta title="Spotify" description="mao">
-                    <template #avatar>
-                        <a-avatar shape="square">
-                            <img alt="avatar" src="/images/spotify-icon.png" />
                         </a-avatar>
                     </template>
                 </a-list-item-meta>
